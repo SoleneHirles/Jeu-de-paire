829a1df4f52de060ee4e8c91a26d1838626f0664
SoleneHirles <solene.hirles@gmail.com>
2021-06-30 23:10:59 +0200

    Selection des bons fichiers

--- memory.py
+++ memory.py
@@ -1,45 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 25 16:31:11 2019
+Created on Sat Oct 26 11:57:17 2019
 
-@author: solen
+@author: solene
 """
 
 import numpy as np
 
-def cartesExistent(a,b):
-    while(a<0) or (a>jeu.size-1) or (b<0) or (b>jeu.size-1) or (a==b):
-        a=int(input("Entrer le numéro de la première carte choisie (entre 1 et 23), Merci"))
-        b=int(input("Entrer le numéro de la seconde carte choisie (entre 1 et 23), Merci"))
-
 def carteExiste(carte1, carte2):
     while(carte1<0) or (carte1>jeu.size-1) or (carte1==carte2):
-        carte1=int(input("Entrer le numéro de la carte choisie (entre 1 et 23), Merci"))
-       
+        carte1=int(input("Entrer le numéro de la carte choisie (entre 0 et 23), Merci"))
+    return carte1
+
+def carteTrouve(carte):
+    while jeu[carte]==-10:
+        carte=int(input("paire déjà trouvée, entrer le numéro de d'autres cartes"))
+    return carte
+
+
 jeu1= np.arange(1,13)
-jeu2= np.copy(jeu1)
-jeu1,jeu2=np.random.permutation(jeu1),np.random.permutation(jeu2)
-jeu=np.concatenate((jeu1,jeu2))
+jeu=np.concatenate((jeu1,jeu1))
+jeu=np.random.permutation(jeu)
 
 paire=0
+essai=0
 while paire!=12:
     a,b=-1,-1
-    cartesExistent(a,b)
-    while jeu[a]==-10 or jeu[b]==-10:
-        if jeu[a]==-10 or jeu[b]==-10:
-            a=int(input("paire déjà trouvée, entrer le numéro de d'autres cartes"))
-            b=int(input("paire déjà trouvée, entrer le numéro de d'autres cartes"))
-            cartesExistent(a,b)
-        elif jeu[a]==-10:
-            a=int(input("Entrer une carte qui n'a pas encore été trouvé"))
-            carteExiste(a,b)
-        else:
-            b=int(input("Entrer une carte qui n'a pas encore été trouvé"))
-            carteExiste(b,a)
+    a=carteTrouve(carteExiste(a,b))
+    print("carte1= ", jeu[a])
+    b=carteTrouve(carteExiste(b,a))
+    print("carte2= ",jeu[b], "\n")
+
     if jeu[a]==jeu[b]:
         paire=paire+1
-        print("carte1= ", jeu[a], "et carte2= ", jeu[b])
-        print("PAIRE")
+        print("PAIRE \n", paire,"paires trouvées ")      
         jeu[a],jeu[b]=-10,-10
     else:
-        print("carte1= ", jeu[a], "et carte2= ", jeu[b])
+        print("Essayer à nouveau \n", paire, " paires trouvées" )
+    essai=essai+1
+    print("---------------------------------------------")
+print("FIN DU JEU, vous avez mis ",essai," essais pour trouver ", paire," paires")
+    
